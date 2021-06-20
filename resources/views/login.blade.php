d7f8ae735d1b129c0b3f72caec31a465c00a8dba
GaitanDenia <84738385+GaitanDenia@users.noreply.github.com>
2021-06-20 15:36:44 -0600

    Cambios Denia 20/JUN

---
+++ resources/views/login.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <meta charset="UTF-8">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-+0n0xVW2eSR5OomGNYDnhzAbDsOXxcvSN1TPprVMTNDbiYZCxYbOOl7+AMvyTG2x" crossorigin="anonymous">
+
+
+    <style>
+
+
+.abs-center {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+  color: white;
+
+}
+
+.form {
+  width: 300px;
+  height: 350px;
+
+}
+
+body{
+            background-color: blueviolet;}
+
+.titulo{
+  text-align: center;
+}
+
+.login{
+  text-align: center;
+}       
+               
+ </style>
+
+    <title>MedicalRecord</title>
+
+    <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+</head>
+<body > 
+
+<div class="titulo">
+       <label texa>MedicalRecord</label></td>
+ </div>
+
+<div class="container">
+
+  <div class="abs-center">
+    <form action="#" class="border p-3 form">
+<div class="login">
+      <label>LOGIN</label>
+</div>
+     <br>
+     <br>
+      <div class="form-group">
+        <label for="id">Usuario</label>
+        <input type="id" name="id" id="id" class="form-control">
+      </div>
+      <br>
+      <div class="form-group">
+        <label for="password">Contraseña</label>
+        <input type="password" name="password" id="password" class="form-control">
+      </div>
+      <br>
+      <button type="submit" class="btn btn-primary">Ingresar</button>
+    </form>
+  </div>
+</div>
+
+
+
+</body>
+</html>
