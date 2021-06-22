4517ffa0c27a7a56fc7a74be6015805dc19f345f
annergarcia <annergarcia@unah.hn>
2021-06-21 21:20:29 -0600

    correccion
    Se elimino la view formulario.blade, las rutas /login, /doctor y se agrego la turta /welcome.

--- routes/web.php
+++ routes/web.php
@@ -14,8 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('login');
-});
-Route::get('/doctor', function () {
-    return view('formulario');
+    return view('welcome');
 });
